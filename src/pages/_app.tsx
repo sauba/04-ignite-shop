a1a8044246bf330ab8125e0129c6a6079bcbbc59
Thiago José de Queiroz <thiagojqueiroz@outlook.com>
2022-11-12 15:28:19 -0300

    First Commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import Link from "next/link";
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
+
+import Image from 'next/image'
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Link href={'/'}>
+          <Image src={logoImg} alt="" />
+        </Link>
+        
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
